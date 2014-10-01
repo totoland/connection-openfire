8259122d277f9cb2491ee889560c183ea1e5dd2e
totoland29@gmail.com <totoland29@gmail.com@bb57e00e-5ed9-b90b-70f1-c8de770ec809>
2014-10-01 18:32:21 +0000

    commit

---
+++ trunk/ConnectionWeb/src/main/java/com/totoland/web/utils/WebConstant.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.totoland.web.utils;
+
+/**
+ *
+ * @author Totoland
+ */
+public class WebConstant {
+    public static String MESSAGES_PROP = "com/etc/web/resources/messages";
+    public static String CONF_PROP = "conf";
+}
